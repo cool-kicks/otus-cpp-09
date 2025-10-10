5be7cbc3735f8cff1f8132ba738c073c03e8ad9f
user1 <user1name@yandex.ru>
2025-10-11 02:39:48 +0300

    homework02

---
+++ hello-world/homework_source-12995-b41e84/build/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/example.cpp.obj"
+  "CMakeFiles/hello_world.dir/example.cpp.obj.d"
+  "CMakeFiles/hello_world.dir/hello_world.cpp.obj"
+  "CMakeFiles/hello_world.dir/hello_world.cpp.obj.d"
+  "CMakeFiles/hello_world.dir/move_me_out.cpp.obj"
+  "CMakeFiles/hello_world.dir/move_me_out.cpp.obj.d"
+  "hello_world.exe"
+  "hello_world.exe.manifest"
+  "hello_world.pdb"
+  "libhello_world.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
