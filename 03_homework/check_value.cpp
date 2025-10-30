ea4b4e94cd9c98e26f36619ca261f51e7a138459
user1 <user1name@yandex.ru>
2025-10-30 19:01:33 +0300

    homework_03

---
+++ 03_homework/check_value.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "check_value.h"
+#include "random_value.h"
+
+int check_foo() {
+
+	
+	
+	int rv = random_value_foo();
+	//const int target_value = 54;
+	int current_value = 0;
+	//bool not_win = true;
+	int count_attempt = 0;
+
+	std::cout << "Enter your guess:" << std::endl;
+
+	do {
+		std::cin >> current_value;
+
+		if (current_value < rv) {
+			count_attempt++;
+			std::cout << "less than " << current_value << std::endl;
+		}
+		else if (current_value > rv) {
+			count_attempt++;
+			std::cout << "greater than " << current_value << std::endl;
+		}
+		else {
+			count_attempt++;
+			std::cout << "you win!" << std::endl;
+			break;
+		}
+
+	} while(true);
+
+	return count_attempt;
+}
